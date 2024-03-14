3141fcdea57da3b62093fa587d6e8f8a4cb2288a
makarenkoj <makarenkoj53@gmail.com>
2024-03-14 11:40:44 +0200

    add some teste

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe ApplicationHelper, type: :helper do
       it { is_expected.to_not match(%r{<a(.*)>next</a>}) }
     end
   end
+
+  describe 'emoji_flag' do
+    it 'return correct flag' do
+      expect(helper.emoji_flag('ua')).to eql "🇺🇦"
+    end
+  end
+
+  describe 'date format' do
+    it 'return correct format' do
+      expect(helper.date_format(Time.now)).to eql Time.now.strftime('%H:%M %d/%m/%Y')
+    end
+
+    it 'return if object blank' do
+      expect(helper.date_format(nil)).to eql nil
+    end
+  end
 end
