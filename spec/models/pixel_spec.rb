081d5eba59a179dac93179b6c4b7def898db2ae9
makarenkoj <makarenkoj53@gmail.com>
2024-08-30 20:36:03 +0300

    add pixel model tests

--- spec/models/pixel_spec.rb
+++ spec/models/pixel_spec.rb
@@ -1,5 +1,15 @@
+# bundle exec rspec spec/models/pixel_spec.rb
 require 'rails_helper'
 
 RSpec.describe Pixel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'fields' do
+    it { is_expected.to have_db_column(:x).of_type(:integer) }
+    it { is_expected.to have_db_column(:y).of_type(:integer) }
+    it { is_expected.to have_db_column(:color).of_type(:string) }
+    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+  end
+
+  describe 'asociation' do
+    it { should belong_to(:user) }
+  end
 end
