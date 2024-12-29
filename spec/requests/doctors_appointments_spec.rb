bb824c5a753d76680217e0a153144ed4d83395d2
makarenkoj <makarenkoj53@gmail.com>
2024-12-29 14:32:38 +0200

    add appointment update test

--- spec/requests/doctors_appointments_spec.rb
+++ spec/requests/doctors_appointments_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe '/doctors_appointments', type: :request do
 
       expect(response).to have_http_status(:found)
       expect(response).to redirect_to(new_user_session_url)
+      expect(flash['alert']).to eql('You need to sign in or sign up before continuing.')
     end
   end
 
@@ -44,6 +45,7 @@ RSpec.describe '/doctors_appointments', type: :request do
             .strftime('%H:%M %d/%m/%Y')).to eq(params[:doctors_appointment][:visit_time].strftime('%H:%M %d/%m/%Y'))
       expect(current_user.patient_profile.doctors_appointments.last.doctor_profile).to eq(doctor.doctor_profile)
       expect(current_user.patient_profile.doctors_appointments.last.description).to eq(nil)
+      expect(flash['notice']).to eql('Record saved')
     end
 
     it 'not doctor' do
@@ -57,6 +59,7 @@ RSpec.describe '/doctors_appointments', type: :request do
       expect(response).to have_http_status(:found)
       expect(response).to redirect_to(doctor_profiles_url)
       expect(current_user.patient_profile.doctors_appointments.size).to eq(0)
+      expect(flash['notice']).to eql('there is no such doctor')
     end
 
     it 'user not autorize' do
@@ -69,11 +72,33 @@ RSpec.describe '/doctors_appointments', type: :request do
       expect(response).to have_http_status(:found)
       expect(response).to redirect_to(new_user_session_url)
       expect(current_user.patient_profile.doctors_appointments.size).to eq(0)
+      expect(flash['alert']).to eql('You need to sign in or sign up before continuing.')
     end
   end
 
   describe 'PATCH /update' do
-    # TODO
+    it 'successful update' do
+      sign_in current_user
+      appointment = create(:doctors_appointment, patient_profile: current_user.patient_profile)
+      params = { doctors_appointment: { description: 'Update now description', visit_time: Time.current + 2.days } }
+
+      put "/doctors_appointments/#{appointment.id}", params: params
+
+      expect(response).to have_http_status(:found)
+      expect(current_user.patient_profile.doctors_appointments.last.reload.description).to eq('Update now description')
+      expect(flash['notice']).to eql('You have updated the appointment')
+    end
+
+    it 'user not autorize' do
+      appointment = create(:doctors_appointment, patient_profile: current_user.patient_profile)
+      params = { doctors_appointment: { description: 'Update now description', visit_time: Time.current + 2.days } }
+
+      put "/doctors_appointments/#{appointment.id}", params: params
+
+      expect(response).to have_http_status(:found)
+      expect(current_user.patient_profile.doctors_appointments.last.reload.description).not_to eq('Update now description')
+      expect(flash['alert']).to eql('You need to sign in or sign up before continuing.')
+    end
   end
 
   describe 'DELETE /destroy' do
